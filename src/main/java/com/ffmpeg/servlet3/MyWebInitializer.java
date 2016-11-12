0098c8a18d79a188778e1fec254faeb0e03017b5
deejay <dee.jay1@gmail.com>
2016-11-12 13:49:41 +0000

    first draft

---
+++ src/main/java/com/ffmpeg/servlet3/MyWebInitializer.java
@@ -0,0 +1,25 @@
+package com.ffmpeg.servlet3;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.ffmpeg.config.SpringWebConfig;
+
+public class MyWebInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+}
---
+++ src/main/java/com/ffmpeg/web/jsonview/Views.java
@@ -0,0 +1,5 @@
+package com.ffmpeg.web.jsonview;
+
+public class Views {
+    public static class Public {}
+}
